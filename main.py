9eb87ad81c13ec0264fd89f5fef5cb4ba6c3a3fd
red5191 <the_red51@outlook.com>
2024-10-21 06:05:35 +0300

    ver2

--- main.py
+++ main.py
@@ -1,64 +1,42 @@
 print("Пройдите простой тест чтобы узнать можно ли вам голосовать.", '\n')
 
+age = 'Вы старше 18? '
+citizenship = 'У вас актуальное гражданство? '
+disqualified = 'Вы освобождены от уголовного преследования? '
+again = 'Ещё раз? '
+
+QAs_list = [age, citizenship, disqualified, again]
+QAs_dict = {
+    age: 'Вы слишком молоды и не можете голосовать',
+    citizenship: 'Вы не гражданин, и не можете голосовать.',
+    disqualified: 'Вам запрещено голосовать до прекращения уголовного преследования',
+    again: 'Тестирование завершено.'
+}
+
+
+def test(QA):
+    correct = ['да', 'нет']
+    answer = input(QA).lower()
+    while answer not in correct:
+        print('Пожалуйста используйте для ответа \"да\" или \"нет\"')
+        answer = input(QA).lower()
+    if answer == 'да':
+        return True
+
 
 def vote_test():
-    def age_test():
-        while True:
-            try:
-                age = int(input("Введите количество полных лет "))
-                break
-            except ValueError:
-                print('Пожалуйста используйте целое число')
-        return age
-
-    def citizenship_test():
-        correct = ['да', 'нет']
-        citizenship = input("Вы гражданин? ")
-        while citizenship not in correct:
-            print('Пожалуйста используйте для ответа \"да\" или\"нет\"')
-            citizenship = input("Вы гражданин? ")
-        return citizenship
-
-    def disqualified_test():
-        correct = ['да', 'нет']
-        disqualified = input("Вы находитесь под уголовным преследованием? ")
-        while disqualified not in correct:
-            print('Пожалуйста используйте для ответа \"да\" или \"нет\"')
-            disqualified = input("Вы находитесь под уголовным преследованием? ")
-        return disqualified
-
-    age_input = age_test()
-    if age_input < 18:
-        print('Вы ещё слишком молоды, и не можете голосовать.', '\n')
-    else:
-
-        citizenship_input = citizenship_test()
-        if citizenship_input != 'да':
-            print('Вы не гражданин, и не можете голосовать.', '\n')
-        else:
-
-            disqualified_input = disqualified_test()
-            if disqualified_input == 'да':
-                print('Вам запрещено голосовать до прекращения уголовного преследования.', '\n')
-            else:
-                print('Поздравляем! Вы можете голосовать.', '\n')
-
+    for n, qa in enumerate(QAs_dict):
+        if not test(qa):
+            print(QAs_dict.get(QAs_list[n]))
+            break
+        elif n == 2:
+            print('Поздравляем! Вы можете голосовать.', '\n')
+            break
 
 vote_test()
 
+while test(again) == True:
+    vote_test()
+else:
+    print(QAs_dict.get(again))
 
-def repeater():
-    while True:
-        correct = ['да', 'нет']
-        repeater_input = input("Ещё раз? ")
-        while repeater_input not in correct:
-            print('Пожалуйста используйте для ответа \"да\" или\"нет\"')
-            repeater_input = input("Ещё раз? ")
-        if repeater_input == 'да':
-            vote_test()
-        elif repeater_input == 'нет':
-            print('Тестирование завершено.')
-            break
-
-
-repeater()
