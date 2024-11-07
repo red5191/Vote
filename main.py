6d3ac7e3843525cfe5c18b3277a59c65339ba65e
red5191 <the_red51@outlook.com>
2024-11-07 19:59:35 +0300

    12345

--- main.py
+++ main.py
@@ -1,90 +1,29 @@
-library = {
-    'Dune' : {'author' : 'Franklin Patrick Herbert', 'year' : 1965, 'availability' : True},
-    'Kolobok' : {'author' : 'Tolstoy Alexey Nikolaevich', 'year' : 1941, 'availability' : False},
-    'Wiedźmin' : {'author' : 'Andrzej Sapkowski', 'year' : 1986, 'availability' : True}
-}
+class BankAccount:
+    def __init__(self):
+        self.__balance = 0
 
-def book_list_view(library):
-    if not library:
-        book_list = 'Библиотека пуста'
-    else:
-        book_list = list(library.keys())
-    return book_list
+    def set_balance(self, deposit, withdraw):
+        try:
+            if deposit >= 0 and withdraw >= 0:
+                self.__balance += float(deposit)
+                print(f'Счет пополнен на {deposit} MNT')
+                if withdraw < self.__balance:
+                    self.__balance -= float(withdraw)
+                    print(f'Со счета было списано {withdraw} MNT')
+                else:
+                    print(f'Нельзя списать больше чем числится на счете')
+            else:
+                print('Депозит и списание должны быть положительными числами')
+        except ValueError:
+            print('Депозит и списание должны быть положительными числами')
 
+    def get_balance(self):
+        return print(f'Баланс по счету составляет {self.__balance} MNT')
 
-#dz2
-print(book_list_view(library))
-
-
-#dz3
-correct = ['да', 'нет']
-def add_book():
-    title = input('Укажите название ')
-    author = input('Укажите автора ')
-    year = input('Укажите год ')
-    if title in library:
-        refresh = input('Обновить информацию? ').lower()
-        while refresh not in correct:
-            print('Пожалуйста используйте для ответа \"да\" или \"нет\"')
-            refresh = input('Обновить информацию? ').lower()
-        if refresh == 'да':
-            library[title] = {'author' : author, 'year' : year, 'availability' : None}
-            print(f'Данные по книге {title} успешно обновлены')
-    else:
-        library[title] = {'author' : author, 'year' : year, 'availability' : None}
-        print(f'Данные по книге {title} успешно добавлены')
+    balance = property(get_balance, set_balance)
 
 
-# add_book()
+myacc = BankAccount()
 
-# dz4
-def remove_book():
-    title = input('Укажите название удаляемой книги ')
-    if title in library:
-        library.pop(title)
-        print(f'Книга {title} успешно удалена')
-    else:
-        print('Такой книги в библиотеке не числится')
-
-# remove_book()
-
-# dz5
-def issue_book():
-    title = input('Какую книгу выдать? ')
-    if title in library:
-        book = library[title]
-        book['availability'] = False
-        print(f'Книга {title} выдана')
-    else:
-        print('Такой книги в библиотеке не числится')
-
-def return_book():
-    title = input('Какую книгу возвращают? ')
-    if title in library:
-        book = library[title]
-        book['availability'] = True
-        print(f'Книга {title} возвращена')
-    else:
-        print('Такой книги в библиотеке не числится')
-
-
-# issue_book()
-# return_book()
-
-#dz6
-def find_book(title):
-    if title in library:
-        book = library[title]
-        print(f'Книга {title} за авторством {book['author']}, {book['year']}-го года выпуска числится в библиотеке')
-        # if book['availability'] is True:
-        #     print('Книга доступна')
-        # if book['availability'] is False:
-        #     print('Книга выдана')
-        # if book['availability'] is None:
-        #     print('Книга в библиотеке, но ее статус не определен')
-
-    else:
-        print('Такой книги в библиотеке не числится')
-
-
-# find_book('Wiedźmin')
+myacc.set_balance(15000, 4567)
+myacc.get_balance()
